b3c03e659eb73629776c84de27166beb99885c2d
srishtiyadav05 <srishtiyadav175@gmail.com>
2025-07-06 05:04:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Toggle navbar style on scroll
+window.addEventListener('scroll', () => {
+  const navbar = document.getElementById('navbar');
+  if (window.scrollY > 60) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
